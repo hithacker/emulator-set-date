ec26b5c418dc941048b884188b32157f4779e235
Hiren Thacker <hithacker@gmail.com>
2020-07-23 15:07:58 +0530

    Update emudate.sh

--- emudate.sh
+++ emudate.sh
@@ -1,6 +1,6 @@
-H=$(date "+%H")
-M=$(date "+%M")
-S=$(date "+%S")
+H=$(gdate "+%H")
+M=$(gdate "+%M")
+S=$(gdate "+%S")
 
 futureDate=$(gdate -d "$1" "+%m%d$H$M%Y.$S")
 result=$(adb -s 192.168.57.101:5555 shell "su 0 toybox date $futureDate")
